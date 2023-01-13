659f2eca4fea62f5aa0dbcc80d96a0becff8dd74
Gustavo Pereira <gustavogp1903@hotmail.com>
2023-01-13 10:45:55 -0300

    Promotion 2

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -41,7 +41,7 @@ public class Program {
 			if(chessMatch.getPromoted() !=null) {
 				System.out.print("Enter piece for promotion (B/N/R/Q): ");
 				String type = sc.nextLine().toUpperCase();
-				if(!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+				while(!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
 					System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
 					type = sc.nextLine().toUpperCase();
 				}
